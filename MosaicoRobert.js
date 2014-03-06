7fd0508250938f0a6195a9c64a24bc65bd0bf605
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2014-03-06 01:59:52 -0300

    bla

--- MosaicoRobert.js
+++ MosaicoRobert.js
@@ -2,49 +2,18 @@ var map = function( x,  in_min,  in_max,  out_min,  out_max){
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
     
-var dibujarBaldosas = function(baldosas){
-    var size_baldosa = $("#size_baldosa").val();
+$(function () { 
+    var $canvas_imagen_original = $("#canvas_imagen_original");
+    var canvas_imagen_original = $canvas_imagen_original[0];
+    var ctx = canvas_imagen_original.getContext("2d");
+    var image = new Image();
+    image.src = "imagen_original.jpg";
+    
+    var btn_generar = $("#btn_generar");
     var contenedor_baldosas = $("#contenedor_baldosas");
     var plantilla_baldosa = $("#plantillas .baldosa");
     var plantilla_pixel = $("#plantillas .pixel");
     var capa_baldosas = $("#capa_baldosas");
-    
-    capa_baldosas.css("width", (Math.ceil(canvas_imagen_original.width/size_baldosa)*size_baldosa).toString()+"px");
-    capa_baldosas.css("height", (Math.ceil(canvas_imagen_original.height/size_baldosa)*size_baldosa).toString()+"px");
-    
-    for(var i=0; i< baldosas.length; i++){
-        var div_baldosa = plantilla_pixel.clone();
-        div_baldosa.css("width", Math.floor(size_baldosa).toString()+"px");
-        div_baldosa.css("height", Math.floor(size_baldosa).toString()+"px");
-        div_baldosa.find("#lbl_color").text(i);                
-        capa_baldosas.append(div_baldosa);
-    }
-    contenedor_baldosas.hide();
-    //for(var i=0; i< baldosas.length; i++){
-    for(var i=0; i< 5; i++){ //debug sacar
-        var div_baldosa = plantilla_baldosa.clone();
-        div_baldosa.find("#numero_baldosa").text(i);
-        var baldosa = baldosas[i];
-        for(var j=0; j<baldosa.length; j++){
-            var div_pixel = plantilla_pixel.clone();
-            div_pixel.css("width", Math.floor(500/size_baldosa).toString()+"px");
-            div_pixel.css("height", Math.floor(500/size_baldosa).toString()+"px");
-            div_pixel.find("#lbl_color").text(baldosa[j]);                
-            div_baldosa.find("#contenedor_pixeles").append(div_pixel);
-        }
-        contenedor_baldosas.append(div_baldosa);
-    }     
-    contenedor_baldosas.show();
-};
-    
-$(function () { 
-    $canvas_imagen_original = $("#canvas_imagen_original");
-    canvas_imagen_original = $canvas_imagen_original[0];
-    ctx = canvas_imagen_original.getContext("2d");
-    image = new Image();
-    image.src = "imagen_original.jpg";
-    
-    var btn_generar = $("#btn_generar");
     
     $(image).load(function() {
         $canvas_imagen_original.attr("width", image.width);
@@ -69,7 +38,33 @@ $(function () {
                     baldosas.push(baldosa);
                 }                  
             }            
-            dibujarBaldosas(baldosas);
+
+            capa_baldosas.css("width", (cant_baldosas_h*size_baldosa).toString()+"px");
+            capa_baldosas.css("height", (cant_baldosas_v*size_baldosa).toString()+"px");
+
+            for(var i=0; i< baldosas.length; i++){
+                var div_baldosa = plantilla_pixel.clone();
+                div_baldosa.css("width", size_baldosa.toString()+"px");
+                div_baldosa.css("height", size_baldosa.toString()+"px");
+                div_baldosa.find("#lbl_color").text(i);                
+                capa_baldosas.append(div_baldosa);
+            }
+            contenedor_baldosas.hide();
+            //for(var i=0; i< baldosas.length; i++){
+            for(var i=0; i< 5; i++){ //debug sacar
+                var div_baldosa = plantilla_baldosa.clone();
+                div_baldosa.find("#numero_baldosa").text(i);
+                var baldosa = baldosas[i];
+                for(var j=0; j<baldosa.length; j++){
+                    var div_pixel = plantilla_pixel.clone();
+                    div_pixel.css("width", Math.floor(500/size_baldosa).toString()+"px");
+                    div_pixel.css("height", Math.floor(500/size_baldosa).toString()+"px");
+                    div_pixel.find("#lbl_color").text(baldosa[j]);                
+                    div_baldosa.find("#contenedor_pixeles").append(div_pixel);
+                }
+                contenedor_baldosas.append(div_baldosa);
+            }     
+            contenedor_baldosas.show();
         });  
         
     });
